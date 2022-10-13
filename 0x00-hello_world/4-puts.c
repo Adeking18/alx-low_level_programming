33ff842f9f377b1e695acb6b9e21f209020dd39d
atundeadekunle <atundeadekunle@gmail.com>
2022-10-13 15:22:59 -0700

    Fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *  main -4put.c
+ *  main -Entry point
  *
  *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	put("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
